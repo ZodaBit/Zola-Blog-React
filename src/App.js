ba081b4c877be708737831e921680be82be3b632
Zelalem Birhan <ZBirhan@excellerentsolutions.com>
2022-12-06 17:46:10 +0300

    Multiple component Unorgnize

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Navbar from './Navbar';
+import Home from './Home';
 function App() {
-  const title='Welecome to Zoda blog';
-  const like=50;
-  const webLink="https://www.google.com"
+  
   return (
     <div className="App">
+      <Navbar></Navbar>
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {like} times</p>
-
-        <p>{10}</p>
-        <p>10</p>
-        <p>{"hallo zoda"}</p>
-        <p>{[1,2,3,4]}</p>
-        <p>{Math.random() *20}</p>
-     <a href={webLink}>Google</a>
+        <Home/>
       </div>
     </div>
   );
