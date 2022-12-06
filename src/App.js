7eda6ddfe952146dd3a7985020775d1f33034207
Zelalem Birhan <ZBirhan@excellerentsolutions.com>
2022-12-06 17:08:32 +0300

    dynamic value react

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title='Welecome to Zoda blog';
+  const like=50;
+  const webLink="https://www.google.com"
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component </h1>
+        <h1>{title}</h1>
+        <p>Liked {like} times</p>
 
+        <p>{10}</p>
+        <p>10</p>
+        <p>{"hallo zoda"}</p>
+        <p>{[1,2,3,4]}</p>
+        <p>{Math.random() *20}</p>
+     <a href={webLink}>Google</a>
       </div>
     </div>
   );
